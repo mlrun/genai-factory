3f57c44606a6eefcc43ad93b755e927aa161e4cd
baaalint <balint_kantor@mckinsey.com>
2024-06-19 14:58:41 +0200

    feat(layout): page transitions

--- src/components/feature/Layout.tsx
+++ src/components/feature/Layout.tsx
@@ -1,11 +1,13 @@
 import { Box, Flex, Menu, MenuItem } from '@chakra-ui/react'
 import { usernameAtom } from 'atoms'
+import { motion as m } from 'framer-motion'
 import { useAtom } from 'jotai'
 import React, { ReactNode } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import Chatbar from './Chatbar'
 import Sidebar from './Sidebar'
 import TopBar from './Topbar'
+
 type LayoutProps = {
   children: ReactNode
 }
@@ -19,29 +21,36 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     navigate('/')
   }
   return (
-    <Flex direction={'column'}>
-      <TopBar user={username} onLoginChange={changeLogin} />
-      <Flex justifyContent={'space-between'}>
-        <Box display={{ base: 'none', md: 'flex' }}>
-          <Sidebar>
-            {pathname === '/chat' ? (
-              <Chatbar />
-            ) : (
-              <Menu>
-                <MenuItem onClick={() => navigate('/admin/users')}>Users</MenuItem>
-                <MenuItem onClick={() => navigate('/admin/chat-histories')}>Chat Histories</MenuItem>
-                <MenuItem>Data Sets</MenuItem>
-                <MenuItem>Documents</MenuItem>
-                <MenuItem>Pipelines</MenuItem>
-              </Menu>
-            )}
-          </Sidebar>
-        </Box>
-        <Flex width={'100%'} height={'91vh'}>
-          {children}
+    <m.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: '100%' }}
+      transition={{ duration: 0.3 }}
+      exit={{ opacity: '50%' }}
+    >
+      <Flex direction={'column'}>
+        <TopBar user={username} onLoginChange={changeLogin} />
+        <Flex justifyContent={'space-between'}>
+          <Box display={{ base: 'none', md: 'flex' }}>
+            <Sidebar>
+              {pathname === '/chat' ? (
+                <Chatbar />
+              ) : (
+                <Menu>
+                  <MenuItem onClick={() => navigate('/admin/users')}>Users</MenuItem>
+                  <MenuItem onClick={() => navigate('/admin/chat-histories')}>Chat Histories</MenuItem>
+                  <MenuItem>Data Sets</MenuItem>
+                  <MenuItem>Documents</MenuItem>
+                  <MenuItem>Pipelines</MenuItem>
+                </Menu>
+              )}
+            </Sidebar>
+          </Box>
+          <Flex width={'100%'} height={'91vh'}>
+            {children}
+          </Flex>
         </Flex>
       </Flex>
-    </Flex>
+    </m.div>
   )
 }
 
