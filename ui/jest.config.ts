6d827b90ff637c1f62102395e133e45f30a2406e
baaalint <balint_kantor@mckinsey.com>
2024-07-04 12:17:52 +0200

    feat(testing): setup testing library

---
+++ ui/jest.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!react-markdown|remark-parse|remark-rehype|rehype-parse|unified|bail|is-plain-obj|trough|vfile|vfile-message).+\\.(js|jsx|ts|tsx)$',
+  ],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/src/__mocks__/fileMock.ts',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@services/(.*)$': '<rootDir>/src/services/$1',
+    '^@shared/(.*)$': '<rootDir>/src/shared/$1',
+    '^@assets/(.*)$': '<rootDir>/src/assets/$1',
+    '^atoms$': '<rootDir>/src/atoms',
+  },
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+};
+
+export default config;
