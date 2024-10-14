8bb5e82bc16af1a2894974588bb1a6361007f646
Nick Schenone <nschenone16@gmail.com>
2024-10-14 09:07:39 -0700

    Set correct application log level based on config (#32)
    * Set correct log level based on config
    
    * Moved log configuration to workflow_server

--- genai_factory/src/genai_factory/workflows/workflow_server.py
+++ genai_factory/src/genai_factory/workflows/workflow_server.py
@@ -49,6 +49,7 @@ class WorkflowServer:
 
     def set_config(self, config: WorkflowServerConfig):
         self._config = config
+        logger.setLevel(config.log_level.upper())
         # reinitialize the controller client with the new config
         self._set_controller_client()
         self._session_store = SessionStore(self._controller_client)
