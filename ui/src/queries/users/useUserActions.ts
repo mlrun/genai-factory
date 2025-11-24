80bf34624edd4cf6be9579be269102f10e14b2bf
Amit Noyberg <amitn@gini-apps.com>
2025-11-24 09:20:32 +0200

    update tables columns

--- ui/src/queries/users/useUserActions.ts
+++ ui/src/queries/users/useUserActions.ts
@@ -1,16 +1,22 @@
-// Copyright 2024 Iguazio
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+Copyright 2024 Iguazio Systems Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License") with
+an addition restriction as set forth herein. You may not use this
+file except in compliance with the License. You may obtain a copy of
+the License at http://www.apache.org/licenses/LICENSE-2.0.
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+implied. See the License for the specific language governing
+permissions and limitations under the License.
+
+In addition, you may not use the software for any purposes that are
+illegal under applicable law, and the grant of the foregoing license
+under the Apache 2.0 license is conditioned upon your compliance with
+such restriction.
+*/
 
 import Client from '@services/Api';
 import { User } from '@shared/types';
@@ -34,7 +40,7 @@ export function useUserActions() {
   });
 
   const updateUser = useMutation({
-    mutationFn: (user: User) =>
+    mutationFn: (user: Partial<User>) =>
       validateApiResponse<User>(
         Client.updateUser(user),
         `update (${user.name})`,
