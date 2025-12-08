2a8e0e28b195596c2f936a3158a55a28986a4ccf
Amit Noyberg <amitn@gini-apps.com>
2025-12-08 09:23:04 +0200

    add form validations

--- ui/src/components/shared/AddEditModal.tsx
+++ ui/src/components/shared/AddEditModal.tsx
@@ -1,18 +1,24 @@
-// Copyright 2024 Iguazio
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+Copyright 2024 Iguazio Systems Ltd.
 
-import React, { useEffect, useState } from 'react';
+Licensed under the Apache License, Version 2.0 (the "License") with
+an addition restriction as set forth herein. You may not use this
+file except in compliance with the License. You may obtain a copy of
+the License at http://www.apache.org/licenses/LICENSE-2.0.
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+implied. See the License for the specific language governing
+permissions and limitations under the License.
+
+In addition, you may not use the software for any purposes that are
+illegal under applicable law, and the grant of the foregoing license
+under the Apache 2.0 license is conditioned upon your compliance with
+such restriction.
+*/
+
+import React from 'react';
 
 import { Button } from '@components/shared/Button';
 import {
@@ -23,7 +29,12 @@ import {
   DialogHeader,
   DialogTitle,
 } from '@components/shared/Dialog';
-import { Field, FieldLabel } from '@components/shared/Field';
+import {
+  Field,
+  FieldContent,
+  FieldError,
+  FieldLabel,
+} from '@components/shared/Field';
 import { Input } from '@components/shared/Input';
 import {
   Select,
@@ -34,6 +45,9 @@ import {
 } from '@components/shared/Select';
 import { Separator } from '@components/shared/Separator';
 import { ModalField } from '@shared/types/modalFieldConfigs';
+
+import { useFormFields } from '@hooks/useFormFields';
+import { cn } from '@shared/cn/utils';
 
 type AddEditModalProps<T> = {
   isOpen: boolean;
@@ -52,76 +66,98 @@ const AddEditModal = <T extends { uid?: string }>({
   onSave,
   title,
 }: AddEditModalProps<T>) => {
-  const [formData, setFormData] = useState<T>(entity);
-
-  useEffect(() => {
-    setFormData(entity);
-  }, [entity]);
-
-  const handleChange = (name: keyof T, value: string) => {
-    setFormData((prev) => ({ ...prev, [name]: value }));
-  };
-
-  const isSaveDisabled = fields.some(
-    (field) =>
-      field.required &&
-      !(formData[field.name as keyof T] ?? '').toString().trim(),
-  );
+  const {
+    errors,
+    formData,
+    handleBlur,
+    handleChange,
+    isSaveDisabled,
+    resetForm,
+  } = useFormFields(entity, fields);
 
   const handleSubmit = () => {
-    onSave(formData);
+    if (!isSaveDisabled) {
+      onSave(formData);
+      onClose();
+      resetForm();
+    }
+  };
+
+  const handleClose = () => {
     onClose();
+    resetForm();
   };
 
   return (
-    <Dialog open={isOpen} onOpenChange={(open) => !open && onClose()}>
-      <DialogContent>
+    <Dialog open={isOpen} onOpenChange={handleClose}>
+      <DialogContent aria-describedby={undefined}>
         <DialogHeader>
           <DialogTitle>
             {entity?.uid ? 'Edit' : 'New'} {title}
           </DialogTitle>
         </DialogHeader>
         <Separator />
-        <div className="grid gap-7 p-8 max-h-[500px] 2xl:max-h-[700px] overflow-auto">
+        <form
+          className="grid gap-7 p-8 max-h-[500px] 2xl:max-h-[700px] overflow-auto"
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit();
+          }}
+          autoComplete="off"
+        >
           {fields.map((field) => (
             <Field key={field.name}>
-              <FieldLabel className="text-black gap-x-1" htmlFor={field.name}>
+              <FieldLabel htmlFor={field.name}>
                 {field.label}
                 {field.required && <span className="text-red-500">*</span>}
               </FieldLabel>
-              {field.options ? (
-                <Select
-                  value={(formData[field.name as keyof T] as string) || ''}
-                  name={field.name}
-                  onValueChange={(value) =>
-                    handleChange(field.name as keyof T, value)
-                  }
-                >
-                  <SelectTrigger>
-                    <SelectValue />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {field.options.map((option) => (
-                      <SelectItem key={option.value} value={option.value}>
-                        {option.label}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-              ) : (
-                <Input
-                  id={field.name}
-                  type="text"
-                  name={field.name}
-                  value={(formData[field.name as keyof T] as string) || ''}
-                  onChange={(e) =>
-                    handleChange(field.name as keyof T, e.target.value)
-                  }
-                />
-              )}
+
+              <FieldContent className="relative">
+                {field.options ? (
+                  <Select
+                    value={(formData[field.name as keyof T] as string) || ''}
+                    name={field.name}
+                    onValueChange={(value) =>
+                      handleChange(field.name as keyof T, value)
+                    }
+                    required={field.required}
+                  >
+                    <SelectTrigger>
+                      <SelectValue />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {field.options.map((option) => (
+                        <SelectItem key={option.value} value={option.value}>
+                          {option.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                ) : (
+                  <Input
+                    type={field.type || 'text'}
+                    name={field.name}
+                    value={(formData[field.name as keyof T] as string) || ''}
+                    onChange={(e) =>
+                      handleChange(field.name as keyof T, e.target.value)
+                    }
+                    onBlur={(e) => handleBlur(field, e.target.value)}
+                    required={field.required}
+                    className={cn(
+                      errors[field.name] && 'border-red-500 bg-red-100',
+                    )}
+                  />
+                )}
+
+                {errors[field.name] && (
+                  <FieldError className="absolute -bottom-2/4">
+                    {errors[field.name]}
+                  </FieldError>
+                )}
+              </FieldContent>
             </Field>
           ))}
-        </div>
+        </form>
 
         <DialogFooter className="flex justify-end gap-2 pt-20 pb-6 px-8">
           <Button disabled={isSaveDisabled} onClick={handleSubmit}>
--- ui/src/constants/userFields.ts
+++ ui/src/constants/userFields.ts
@@ -1,21 +1,42 @@
-// Copyright 2024 Iguazio
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+Copyright 2024 Iguazio Systems Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License") with
+an addition restriction as set forth herein. You may not use this
+file except in compliance with the License. You may obtain a copy of
+the License at http://www.apache.org/licenses/LICENSE-2.0.
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+implied. See the License for the specific language governing
+permissions and limitations under the License.
+
+In addition, you may not use the software for any purposes that are
+illegal under applicable law, and the grant of the foregoing license
+under the Apache 2.0 license is conditioned upon your compliance with
+such restriction.
+*/
 
 import { ModalField } from '@shared/types/modalFieldConfigs';
 
 export const userFields: ModalField[] = [
-  { name: 'name', label: 'Username', required: true },
-  { name: 'email', label: 'Email' },
-  { name: 'full_name', label: 'Full Name' },
+  {
+    name: 'name',
+    label: 'Username',
+    required: true,
+  },
+  {
+    name: 'email',
+    label: 'Email',
+    type: 'email',
+    validation: {
+      regex: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+      message: 'Please enter a valid email address',
+    },
+  },
+  {
+    name: 'full_name',
+    label: 'Full Name',
+  },
 ];
